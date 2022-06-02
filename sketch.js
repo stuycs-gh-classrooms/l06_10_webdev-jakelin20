86c0bd09efaee92a9a14319ca3edbc47d9ab86d8
jakelin20 <90660992+jakelin20@users.noreply.github.com>
2022-06-02 15:01:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,13 +7,14 @@ function setup() {
   createCanvas (400,400);
   background (255);
   frameRate (60);
+  cy=newY(100,height/2,theta);  
+  cx=newX(100,width/2,theta);
 }
 
 function draw() {
   var gradient;
   background (255);
-  cy=newY(100,height/2,theta);  
-  cx=newX(100,width/2,theta);
+  
   gradient=(theta2/60)*10;
   if (gradient>255) {
       theta2=0;
@@ -25,9 +26,6 @@ function draw() {
   cx++;
   theta++;
   theta2++;
-  println (gradient);
-  println("deg: " + theta + "/trad: " + radians (theta));
-
 }
 
 var newY(amplitude, offset, t) { 
